ad78e7de840628bf806977787253f1b0cb26f346
Nguyen Tuan <nnanhtuan410@gmail.com>
2021-08-10 16:08:30 +0700

    update parralax animation

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,9 @@ $(document).ready(function(){
     $('#navtoggle').click(function(){
         if($(this).is(":checked")){
             $('body').addClass("fixed-body");
-            console.log("checked")
         }
         else if($(this).is(":not(:checked)")){
             $('body').removeClass("fixed-body");
-            console.log("unchecked")
         }
     });
 
@@ -24,40 +22,52 @@ $(document).ready(function(){
         $('#nav').removeClass('fixed-menu');
     }
     })
-    //paralax bg
-    $('.parallax-window').parallax({imageSrc: './images/recruit-img.png'});
-
+    //parralax animation
+    $(window).scroll(function(){
+        var scrollTop = $(this).scrollTop();
+        console.log(scrollTop);
+        $('.parralax').css('top', - ((scrollTop /10)*1.2 + -260)+'px');
+    })
     // scroll top btn
     $('#btn-scrollTop').click(function(){
 		$('html,body').animate({
 			scrollTop: 0
 	    }, 500);
 	});
-
     //nav scroll content
     $("#service-link").click(function() {
         $('html,body').animate({
             scrollTop: $("#service").offset().top -80
         }, 1000);
+        $('#navtoggle').prop('checked', false);
+        $('body').removeClass("fixed-body");
     })
     $("#about-link").click(function() {
         $('html,body').animate({
             scrollTop: $("#about").offset().top -80
         }, 1000);
+        $('#navtoggle').prop('checked', false);
+        $('body').removeClass("fixed-body");
     })
     $("#partners-link").click(function() {
         $('html,body').animate({
             scrollTop: $("#partners").offset().top - 80
         }, 1000);
+        $('#navtoggle').prop('checked', false);
+        $('body').removeClass("fixed-body");
     })
     $("#recruit-link").click(function() {
         $('html,body').animate({
             scrollTop: $("#recruit").offset().top -80
         }, 1000);
+        $('#navtoggle').prop('checked', false);
+        $('body').removeClass("fixed-body");
     })
     $("#content-link").click(function() {
         $('html,body').animate({
             scrollTop: $("#contact").offset().top -80
         }, 1000);
+        $('#navtoggle').prop('checked', false);
+        $('body').removeClass("fixed-body");
     })
 });
